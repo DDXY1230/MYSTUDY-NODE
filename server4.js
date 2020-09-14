8ba3d4e63c53cd960e705f815f16cbfc0b4734c6
DDXY1230 <2502752288@qq.com>
2020-09-14 13:27:11 +0800

    add file

---
+++ server4.js
@@ -0,0 +1,30 @@
+let http = require('http')
+let fs = require('fs')
+
+let documentRoot = '/Users/mac/Desktop/node学习笔记/test' // 这里是先设置跟路径
+console.log('服务开启了')
+
+let httpServer = http.createServer(function (req, res) {
+    // console.log(req)
+    let url = req.url
+    let file = documentRoot + url // 绝对路径的地址，然后再找到文件
+    console.log(file)
+    // let file = documentRoot // 绝对路径的地址，然后再找到文件
+    fs.readFile(file, function (err, data) {
+        if (err) {
+            console.log('读取文件出错了')
+            res.writeHeader(404, {
+                'content-type': 'text/html;charset="utf-8"'
+            })
+            res.write('<h1>你要找的页面被牛吃了</h1>')
+            res.end()
+        } else {
+            res.writeHeader(200, {
+                'content-type': 'text/html;charset="utf-8"'
+            })
+            res.write(data)
+            res.end()
+        }
+
+    })
+}).listen(8880)
