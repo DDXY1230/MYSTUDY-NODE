3c9726f107f119357c7e03805f01cc50eeab179f
DDXY1230 <2502752288@qq.com>
2020-09-22 14:21:09 +0800

    koa  测试

--- 02lesson/index.js
+++ 02lesson/index.js
@@ -1,10 +1,11 @@
 const Koa = require('koa')
 const app = new Koa()
 app.use((ctx, next) => {
-    console.log('ctx', ctx)
+    console.log('ctx===》', ctx)
     ctx.body = {
         name: '小田田在3000端口启动了一个服务'
     }
+    console.log('ctx==>2', ctx)
 })
 app.listen(3000, () => {
     console.log('server start at 3000')
