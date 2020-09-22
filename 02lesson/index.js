7ae7e5a75f4ba138c1b1648bc58e749d6e834518
DDXY1230 <2502752288@qq.com>
2020-09-22 14:34:35 +0800

    fix

--- 02lesson/index.js
+++ 02lesson/index.js
@@ -1,7 +1,21 @@
 const Koa = require('koa')
 const app = new Koa()
+
+app.use(async (ctx, next) => {
+    const start = Date.now()
+    await next()
+    console.log('下一个执行完了')
+    const end = Date.now()
+    // console.log('start', start)
+    console.log('请求耗时：', end - start)
+})
+
+
 app.use((ctx, next) => {
     console.log('ctx===》', ctx)
+    console.log('开始执行了啊')
+    const expire = Date.now() + 100
+    while (Date.now() < expire) {}
     ctx.body = {
         name: '小田田在3000端口启动了一个服务'
     }
