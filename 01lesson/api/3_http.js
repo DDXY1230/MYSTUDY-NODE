77a431486242a8f23fcfb49e7eec4d9fda59862d
DDXY1230 <2502752288@qq.com>
2020-09-27 14:31:55 +0800

    基本的服务起路由

--- 01lesson/api/3_http.js
+++ 01lesson/api/3_http.js
@@ -9,10 +9,24 @@ const server = http.createServer((request, response) => {
         fs.readFile('./1.js', (err, data) => {
             if(err) {
                 console.log('读取文件失败')
+                response.writeHead(500, {'Content-Type': 'text/plain;charset=utf-8'})
+                response.end('500 服务器错误 哈哈')
             }else {
+                response.statusCode = 200
+                response.setHeader('Content-Type','text/html')
                 response.end(data)
             }
         })
+    }
+    else if(url === '/users' && method === 'GET'){
+        response.writeHead(200, {'Content-Type': 'application/json'})
+        // response.end返回的必须是一个字符串，所以在这里要用JSON.stringify()
+        response.end(JSON.stringify({name: 'tom',age: 18}))
+    }    
+    else {
+        response.statusCode = 404
+        response.setHeader('Content-Type', 'text/html')
+        response.end('404 nopage')
     }
 })
 server.listen(3000)
